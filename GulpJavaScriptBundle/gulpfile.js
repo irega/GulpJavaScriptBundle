cd160aa728865d5769ed5f06719829ba109a2990
Ivan Reinoso <ivan.reinoso@outlook.es>
2016-03-14 19:06:39 +0100

    Simplifico código de gulpfile.js

--- GulpJavaScriptBundle/gulpfile.js
+++ GulpJavaScriptBundle/gulpfile.js
@@ -2,23 +2,11 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var fs = require('fs');
 
-var paths = {
-    js_files: "./src/js/**/*.js",
-    js_entryPoint: "./src/js/lib.js"
-};
-
-var bundler = browserify(paths.js_entryPoint);
+var bundler = browserify("./src/js/lib.js");
 
 gulp.task('bundle-js', function () {
     bundler.bundle()
-    .on('error', function (err) { console.error(err); })
     .pipe(fs.createWriteStream('bin/miLibreria.js'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(paths.js_files, ['bundle-js']);
-});
-
 gulp.task('default', ['bundle-js']);
-
-gulp.task('start', ['bundle-js', 'watch']);
